2d56bc7051016b2420813995d31f995cc54dde48
simonchou <2484152300@qq.com>
2021-06-06 20:09:56 +0800

    base64编码图片，完善一些功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,14 +18,14 @@ Page({
    */
   onLoad: function () {
     //播放音乐
-    // this.backmusic();
+    this.backmusic();
   },
   backmusic: function () {
     //  音乐播放函数
     player();
     function player() {
       back.title = "A Little Story";   // 必须要有一个title
-      back.src = "http://121.51.180.62/amobile.music.tc.qq.com/C400004L5Djz2Niv2s.m4a?guid=579928027&vkey=F0C7852628ED97A5804CC5B09BABA6E11D7F85DA94FC47870C4375D883235068C816FAAA4FED571A2AB47BF56EF969AE91373369C03DDA14&uin=&fromtag=66"; 
+      back.src = "https://dl.stream.qqmusic.qq.com/C400004L5Djz2Niv2s.m4a?guid=1633874806&vkey=51558FE967CEE99C10C56E34F13F41D2FAF153430D0D4833CE45E8FD5EA060B205CB230390AAB9145675FA99EE1C25B330DE9C7CE376C84F&uin=2484152300&fromtag=66"; 
       back.onEnded(() => {
         player();  // 音乐循环播放
       })
