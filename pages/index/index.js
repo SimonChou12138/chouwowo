6af07b00138b4eec1c4190f0e017261c535b815d
simonchou <2484152300@qq.com>
2021-06-06 12:16:30 +0800

    实现登录功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,32 @@
-
 //index.js
-
+const back = wx.getBackgroundAudioManager();
 
 Page({
   
   data: {
     
-  }
-
+  },
+  showtravelcontent:function(){
+    wx.navigateTo({
+      url: '/pages/travelcontent/travelcontent',
+    })
+  },
+  /**
+   * 加载时
+   */
+  onLoad: function () {
+    //播放音乐
+    // this.backmusic();
+  },
+  backmusic: function () {
+    //  音乐播放函数
+    player();
+    function player() {
+      back.title = "A Little Story";   // 必须要有一个title
+      back.src = "http://121.51.180.62/amobile.music.tc.qq.com/C400004L5Djz2Niv2s.m4a?guid=579928027&vkey=F0C7852628ED97A5804CC5B09BABA6E11D7F85DA94FC47870C4375D883235068C816FAAA4FED571A2AB47BF56EF969AE91373369C03DDA14&uin=&fromtag=66"; 
+      back.onEnded(() => {
+        player();  // 音乐循环播放
+      })
+    }
+  },
 })
--- pages/user/user.js
+++ pages/user/user.js
@@ -1,5 +1,5 @@
 // pages/user/user.js
-
+const app = getApp()
 
 Page({
 
@@ -7,7 +7,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    userInfo: {},
+    attentionNum:"",
+    fans:"",
+  },
+  /**
+   * 用户授权登录
+   */
+  login:function(){
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          attentionNum:6,
+          fans:666,
+          hasUserInfo: true
+        })
+      }
+    })
   }
-
 })
